7cdc372927dca210db4105a41d7cb95c4bc64325
INTEL-COREi5 <INTEL-COREi5@LAPTOP-KJG8HBQ7>
2019-01-14 22:20:59 -0500

    Cualquier

--- script.sql
+++ script.sql
@@ -13,34 +13,32 @@ use rosario;
 create table afiliados(
  idafiliado int auto_increment primary key,
  cedula varchar(10) unique,
- numfiliado varchar(8),
- nombre varchar(40),
- apellido varchar(40) 
+ numfiliado varchar(20),
+ nombre varchar(100),
+ apellido varchar(100) 
 );
 -- -----------------------------------------------------------------
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1208018666','1864271','SHIRLEY','MIRANDA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1205060252','1864272','CARMEN','BARANDIKA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204875212','1864273','PEDRO','ZAMORA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000423','1864274','JUAN','RAMIREZ');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1201141338','1864271','JHONNY PRIMITIVO','ASPIAZU INFANTE');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204556045','1864272','TANIA MARITZA','BARANDICA BUSTAMANTE');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1201274683','1864273','CELSO INOCENTE','BUSTAMANTE SANTILLAN');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204216111','1864274','JUAN ALVERTO','CEREZO ROSADO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1202239768','1864275','RAUL RAMON','FERNADEZ GALLARDO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1200747960','1864276','NICOLAZ MARIO','GALLARDO PINTO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204718629','1864277','YANETH EUGENIA','LOOR MURILLO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204178162','1864278','CARLOS ARMANDO','MUÑOZ CAREGUA');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('0917102808','1864279','ERWIN LODAN','MUÑOZ LITARDO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204253015','1864280','FRANKLIN RUBEN','NUÑEZ FRANCO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204373151','1864281','ELOY LUCIO','OCHOA QUINTO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1206524090','1864282','RUBEN JUSTINO','PINTO GUTIERREZ');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1201760392','1864283','NORTON VIDAL','PLUAS LAJE');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204461436','1864284','EDIN ISRAEL','QUINTO SANTILLAN');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1201972286','1864285','ALVERTO SEGUNDO','ROSADO SANTANA');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1202418073','1864286','JAVIER ALIPIO','SALINAS ALVAREZ');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1202256481','1864287','ALFREDO EVARISTO','SANTILLA MIÑO');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204367054','1864288','FRANCISCO JAVIER','SANTILLAN FLORES');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1204366916','1864289','DARWIN BOLIVAR','SANTILLAN FLOREZ');
+insert into afiliados(cedula,numfiliado,nombre,apellido) values('1202371777','1864290','CESAR MARCELINO','ZURITA AVILA');
 
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1205781562','1864275','STEFANIA','ALVARADO');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000424','1864276','MARTHA','JIMENEZ');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000425','1864277','ZACARIAS','BUSTAMANTE');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000426','1864278','CRISTIAN','TRIANA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000427','1864279','CAROLINA','ZAMORA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000458','1864280','NARCISA','BURGOS');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204070443','1864281','LORENZO','MUÑOZ');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000253','1864282','OLINDA','MUÑOZ');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000473','1864283','BETSY','VELOZ');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204800463','1864284','OVIEDO','VERA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000443','1864285','CHAVELA','ROCAFUERTE');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204700413','1864286','CINDY','SOLANO');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000463','1864287','GISELA','ZAPATA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000413','1864288','RINA','ZAMORA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000823','1864289','ROMEL','PEÑA');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204900423','1864290','JHONY','GALLARDO');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000123','1864291','ZOILA','SANTILLAN');
-insert into afiliados(cedula,nombre,numfiliado,apellido) values('1204000223','1864292','RITA','VALVERDE');
 
 
  
